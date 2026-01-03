d4ee3ffd7ae691f6850a77550235a369eb46ae77
Sanath <sanathkharish@gmail.com>
2026-01-03 23:45:13 +0530

    Fix repository structure by removing nested git metadata

---
+++ CalculatorApp/Services/Calculator.cs
@@ -0,0 +1,18 @@
+namespace CalculatorApp.Services;
+
+using CalculatorApp.Interfaces;
+
+public class Calculator : ICalculator
+{
+    public int Add(int a, int b) => a + b;
+    public int Subtract(int a, int b) => a - b;
+    public int Multiply(int a, int b) => a * b;
+
+    public double Divide(int a, int b)
+    {
+        if (b == 0)
+            throw new DivideByZeroException();
+
+        return (double)a / b;
+    }
+}
